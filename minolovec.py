d2ed37ed4b6616c316d343579b6402af0ab99c15
Ines <inesmersak@gmail.com>
2015-03-03 15:12:58 +0100

    Dodala preverjanje preostalih min pri koncu igre, zacetki GUIja - meni, canvas, mreza.

--- minolovec.py
+++ minolovec.py
@@ -1,3 +1,4 @@
+from tkinter import *
 import random
 
 
@@ -21,21 +22,56 @@ class Polje():
         if not self.odprto:
             self.prikaz = 'f'
             self.flagged = True
+            return True
+        return False
 
     def odznaci(self):
         if not self.odprto:
             self.prikaz = '_'
             self.flagged = False
+            return True
+        return False
+
+    def prikaz(self):
+        pass
 
 
 class Minesweeper():
-    def __init__(self, velikost, mine):
+    def __init__(self, master, velikost, mine):
         self.velikost = velikost
+        self.kvadratek = 30
         self.mine = mine
         self.polje = [[Polje(j, i) for i in range(self.velikost)] for j in range(self.velikost)]
         self.preostale_mine = self.mine
-        self.zmage = 0
-        self.porazi = 0
+        self.zmage = IntVar(0)
+        self.porazi = IntVar(0)
+
+        # --- GUI ---
+        master.title('Minolovec')
+
+        okvir = Frame(master)
+        okvir.grid()
+
+        menu = Menu(master)
+        master.config(menu=menu)
+        podmenu = Menu(menu)
+        podmenu.add_command(label='Nova igra', command=self.nova_igra)
+        podmenu.add_command(label='Izhod', command=self.quit)
+        menu.add_cascade(label='File', menu=podmenu)
+
+        Label(okvir, text='Zmage: ').grid(row=0, column=0)
+        Label(okvir, textvariable=self.zmage).grid(row=0, column=1, sticky='W')
+        Label(okvir, text='Porazi: ').grid(row=1, column=0)
+        Label(okvir, textvariable=self.porazi).grid(row=1, column=1, sticky='W')
+
+        self.platno = Canvas(okvir, width=self.velikost*self.kvadratek, height=self.velikost*self.kvadratek,
+                             background='#FFFFFF')
+        self.platno.grid(row=2, column=0, columnspan=2)
+        for i in range(1, self.velikost):
+            self.platno.create_line(i*self.kvadratek, 0, i*self.kvadratek, self.velikost*self.kvadratek)
+            self.platno.create_line(0, i*self.kvadratek, self.velikost*self.kvadratek, i*self.kvadratek)
+        self.platno.bind("<Button-1>", self.levi_klik)
+        self.platno.bind("<Button-3>", self.desni_klik)
 
     def spremeni_stevilko_polj(self, x, y):
         """ Spremeni stevilko polj okoli mine, ta je podana s koordinatami x in y. """
@@ -57,16 +93,26 @@ class Minesweeper():
             self.spremeni_stevilko_polj(x, y)
             i -= 1
 
-    def prikazi_celotno_polje(self, odkrito=False):
-        """ Prikaze celotno polje min in praznih kvadratkov. """
-        niz = ''
-        for x in self.polje:
-            niz += '|'
-            for y in x:
-                n = y.vrednost if odkrito else str(y)
-                niz += ' {0} |'.format(n)
-            niz += '\n'
-        print(niz)
+    # def prikazi_celotno_polje(self, odkrito=False):
+    #     """ Prikaze celotno polje min in praznih kvadratkov. """
+    #     niz = ''
+    #     for x in self.polje:
+    #         niz += '|'
+    #         for y in x:
+    #             n = y.vrednost if odkrito else str(y)
+    #             niz += ' {0} |'.format(n)
+    #         niz += '\n'
+    #     print(niz)
+
+    def levi_klik(self, klik):
+        y = klik.x // self.kvadratek
+        x = klik.y // self.kvadratek
+        print(x, y)
+
+    def desni_klik(self, klik):
+        y = klik.x // self.kvadratek
+        x = klik.y // self.kvadratek
+        print(x, y)
 
     def odpri_blok(self, koord):
         """ Sprejme tuple koord s koordinatami, kamor je uporabnik levo-kliknil (kjer je prazno polje), in odpre vsa
@@ -86,8 +132,9 @@ class Minesweeper():
         """ Sprejme koordinate, kamor je kliknil uporabnik, in ali je oznacil mino ali ne ter posodobi igro,
         tako da odpre polja oziroma oznaci mino. """
         if m:
-            self.polje[x][y].oznaci()
-            self.preostale_mine -= 1
+            ozn = self.polje[x][y].oznaci()
+            if ozn:
+                self.preostale_mine -= 1
         else:
             if self.polje[x][y].vrednost == 'x':
                 return False
@@ -104,29 +151,42 @@ class Minesweeper():
                     return False
         return True
 
-    def igra(self):
-        while True:
-            self.prikazi_celotno_polje()
-            print("Preostale mine:", self.preostale_mine)
-            v = int(input("Vrstica: ")) - 1
-            s = int(input("Stolpec: ")) - 1
-            m = input("(m)ina / (p)razno: ")
-            m = (m == 'm')
-            poteka = self.posodobi(v, s, m)
-            if not poteka:
-                self.prikazi_celotno_polje(odkrito=True)
-                print('Zal ste naleteli na mino!')
-                self.porazi += 1
-                break
-            konec = self.konec()
-            if konec:
-                self.prikazi_celotno_polje(odkrito=True)
-                self.zmage += 1
-                print('Zmagali ste!')
-                break
+    def nova_igra(self):
+        pass
+
+    def quit(self):
+        pass
+
+    # def igra(self):
+    #     while True:
+    #         self.prikazi_celotno_polje()
+    #         print("Preostale mine:", self.preostale_mine)
+    #         v = int(input("Vrstica: ")) - 1
+    #         s = int(input("Stolpec: ")) - 1
+    #         m = input("(m)ina / (p)razno: ")
+    #         m = (m == 'm')
+    #         poteka = self.posodobi(v, s, m)
+    #         if not poteka:
+    #             self.prikazi_celotno_polje(odkrito=True)
+    #             print('Zal ste naleteli na mino!')
+    #             self.porazi += 1
+    #             break
+    #         konec = self.konec()
+    #         if konec:
+    #             self.prikazi_celotno_polje(odkrito=True)
+    #             if self.preostale_mine == 0:
+    #                 self.zmage += 1
+    #                 print('Zmagali ste!')
+    #             else:
+    #                 self.porazi += 1
+    #                 print('Izgubili ste!')
+    #             break
 
 
-igrica = Minesweeper(5, 3)
-igrica.napolni()
-igrica.prikazi_celotno_polje(odkrito=True)
-igrica.igra()
+# igrica = Minesweeper(5, 3)
+# igrica.napolni()
+# igrica.prikazi_celotno_polje(odkrito=True)
+# igrica.igra()
+root = Tk()
+igrica = Minesweeper(root, 10, 3)
+root.mainloop()
